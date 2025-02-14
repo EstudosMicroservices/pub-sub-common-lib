d61dfbee2a88142190d38c7a1c9dd7253f4b6888
ed-atj <edmilsonjunior075@gmail.com>
2025-02-13 22:51:12 -0300

    first commit

---
+++ src/test/java/com/microservices/common_lib/CommonLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.common_lib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
